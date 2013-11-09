ca609903216adf4d0b053ea5874115040601f4b5
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2013-11-09 15:00:39 -0600

    Update clients logic

--- clients/client.go
+++ clients/client.go
@@ -1,15 +1,59 @@
 package main
 
 import (
-  "net"
-  "fmt"
+	"crypto/hmac"
+	"crypto/sha256"
+	"flag"
+	"fmt"
+	"io"
+	"math/rand"
+	"net"
+	"time"
 )
 
+var flag_loop = flag.Bool("loop", false, "run in endless loop")
+var flag_verbose = flag.Bool("verbose", false, "be verbose")
+
+var secret_key = "74e6f7298a9c2d168935f58c001bad88"
+
 func main() {
-  serverAddr, err := net.ResolveUDPAddr("udp6", "[::1]:28000")
-  con, err := net.DialUDP("udp", nil, serverAddr)
+	flag.Parse()
 
-  if err != nil { panic(err) }
+	serverAddr, err := net.ResolveUDPAddr("udp6", "[::1]:28000")
+	if err != nil {
+		panic(err)
+	}
 
-  fmt.Fprintf(con, "temp1:100:74e6f7298a9c2d168935f58c001bad88")
+	rand.Seed(time.Now().UnixNano())
+
+	if *flag_loop {
+		for {
+			SendData(serverAddr)
+		}
+	} else {
+		SendData(serverAddr)
+	}
+
+}
+
+func SendData(server *net.UDPAddr) {
+	con, err := net.DialUDP("udp", nil, server)
+	if err != nil {
+		panic(err)
+	}
+	rand_value := rand.Intn(100)
+	data := fmt.Sprintf("%s:%d", "temp1", rand_value)
+	hmac := GenHmac(data)
+
+	fmt.Fprintf(con, "%s:%s", data, hmac)
+	if *flag_verbose {
+		fmt.Printf("%s:%s\n", data, hmac)
+	}
+}
+
+func GenHmac(message string) string {
+	mac := hmac.New(sha256.New, []byte(secret_key))
+	io.WriteString(mac, message)
+
+	return fmt.Sprintf("%x", mac.Sum(nil))
 }
--- clients/client.rb
+++ clients/client.rb
@@ -1,15 +1,45 @@
 #! /usr/bin/env ruby
 
 require 'socket'
+require 'openssl'
+require 'optparse'
 
-probe_name  = 'temp1'
-value       = rand(100)
-hmac        = '74e6f7298a9c2d168935f58c001bad88'
-
-collector = {
+COLLECTOR = {
   :host => '::1',
   :port => 28000
 }
 
-udp = UDPSocket.new(Socket::AF_INET6)
-udp.send "#{probe_name}:#{value}:#{hmac}", 0, collector[:host], collector[:port]
+SECRET_KEY = '74e6f7298a9c2d168935f58c001bad88'
+PROBE_NAME = 'temp1'
+
+SETTINGS = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: client.rb [options]"
+
+  opts.on("-v", "--verbose", "Be verbose") do |v|
+    SETTINGS[:verbose] = v
+  end
+  opts.on("-l", "--loop", "Run in endless loop") do |v|
+    SETTINGS[:loop] = v
+  end
+end.parse!
+
+def send_data
+  udp   = UDPSocket.new(Socket::AF_INET6)
+  value = rand(100)
+  data  = "#{PROBE_NAME}:#{value}"
+  hmac  = gen_hmac(data)
+
+  puts "#{data}:#{hmac}" if SETTINGS[:verbose]
+  udp.send "#{data}:#{hmac}", 0, COLLECTOR[:host], COLLECTOR[:port]
+end
+
+def gen_hmac(data)
+  OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, SECRET_KEY, data)
+end
+
+if SETTINGS[:loop]
+  loop { send_data }
+else
+  send_data
+end
