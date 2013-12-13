41a7212163117c1dd1a27fae505fe7048e89c17b
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2013-12-13 08:07:02 -0600

    Fix indents

--- clients/client.py
+++ clients/client.py
@@ -21,23 +21,23 @@ cmdargs = parser.parse_args()
 sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 
 def send_data():
-  value = random.random() * 100
-  data  = '%(name)s:%(value)f' % {'name': PROBE_NAME, 'value': value}
-  hmac  = gen_hmac(data)
+    value = random.random() * 100
+    data  = '%(name)s:%(value)f' % {'name': PROBE_NAME, 'value': value}
+    hmac  = gen_hmac(data)
 
-  message = '%(data)s:%(hmac)s' % {'data':data, 'hmac':hmac}
+    message = '%(data)s:%(hmac)s' % {'data':data, 'hmac':hmac}
 
-  if cmdargs.verbose:
-    print message
+    if cmdargs.verbose:
+        print message
 
-  sock.sendto(message, (UDP_IP, UDP_PORT))
+    sock.sendto(message, (UDP_IP, UDP_PORT))
 
 def gen_hmac(data):
-  hasher = hmac.new(SECRET_KEY, data, hashlib.sha256)
-  return hasher.hexdigest()
+    hasher = hmac.new(SECRET_KEY, data, hashlib.sha256)
+    return hasher.hexdigest()
 
 if cmdargs.loop:
-  while True:
-    send_data()
+    while True:
+        send_data()
 else:
-  send_data()
+    send_data()
